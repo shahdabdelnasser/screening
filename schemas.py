cea020ee0ae40e56a3de58ce3ef93a171a46b0ce
shahdabdelnasser <35689813+shahdabdelnasser@users.noreply.github.com>
2026-06-21 23:04:11 +0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -2,25 +2,25 @@ from pydantic import BaseModel, Field
 
 
 class InputData(BaseModel):
-    AnxietSev_23: int = Field(..., ge=0, le=3)
-    learning_23: int = Field(..., ge=0, le=1)
-    ASDMed_23: int = Field(..., ge=0, le=1)
+    AnxietSev_23: int 
+    learning_23: int 
+    ASDMed_23: int 
     
-    K8Q21: int = Field(..., ge=0, le=3)
-    DevDelay_23: int = Field(..., ge=0, le=1)
-    ReptGrade_23: int = Field(..., ge=0, le=1)
-    speech_23: int = Field(..., ge=0, le=1)
-    WgtConcn_23: int = Field(..., ge=0, le=3)
-    K7Q70_R: int = Field(..., ge=0, le=1)
-    K2Q32A: int = Field(..., ge=0, le=1)
-    ACE11: int = Field(..., ge=0, le=1)
-    K7Q83_R: int = Field(..., ge=0, le=1)
-    K7Q82_R: int = Field(..., ge=0, le=1)
-    curious6to17_23: int = Field(..., ge=0, le=1)
-    K7Q85_R: int = Field(..., ge=0, le=1)
-    K7Q84_R: int = Field(..., ge=0, le=1)
+    K8Q21: int 
+    DevDelay_23: int 
+    ReptGrade_23: int 
+    speech_23: int 
+    WgtConcn_23: int 
+    K7Q70_R: int
+    K2Q32A: int 
+    ACE11: int 
+    K7Q83_R: int 
+    K7Q82_R: int 
+    curious6to17_23: int 
+    K7Q85_R: int 
+    K7Q84_R: int 
     DiffMem_23:int
-    behavior_23: int = Field(..., ge=0, le=3)
+    behavior_23: int 
     ACE7: int
     ACE10: int
     ACE11:int
@@ -36,27 +36,27 @@ class InputData(BaseModel):
     ACE8:int
     ACE9:int
 
-    MealSkip_23: int = Field(..., ge=0, le=1)
-    GRADES: int = Field(..., ge=0, le=3)
-    MAKEFRIEND: int = Field(..., ge=0, le=1)
+    MealSkip_23: int
+    GRADES: int
+    MAKEFRIEND: int 
 
-    PrntCncrn_23: int = Field(..., ge=0, le=3)
-    SchlMiss_23: int = Field(..., ge=0, le=3)
-    ACE6: int = Field(..., ge=0, le=1)
-    bullied_23: int = Field(..., ge=0, le=1)
+    PrntCncrn_23: int 
+    SchlMiss_23: int
+    ACE6: int 
+    bullied_23: int
     
-    LowInterest_23: int = Field(..., ge=0, le=1)
-    DailyAct_23: int = Field(..., ge=0, le=1)
-    binge_23: int = Field(..., ge=0, le=1)
+    LowInterest_23: int
+    DailyAct_23: int 
+    binge_23: int 
     
-    SC_SEX: int = Field(..., ge=0, le=1)
-    PickyEat_23: int = Field(..., ge=0, le=1)
-    SpeechSev_23: int = Field(..., ge=0, le=3)
+    SC_SEX: int 
+    PickyEat_23: int 
+    SpeechSev_23: int 
     
-    bully_23: int = Field(..., ge=0, le=1)
-    DevDelSev_23: int = Field(..., ge=0, le=3)
-    DepresSev_23: int = Field(..., ge=0, le=3)
-    BehavSev_23: int = Field(..., ge=0, le=3)
+    bully_23: int 
+    DevDelSev_23: int 
+    DepresSev_23: int 
+    BehavSev_23: int 
 
 
     DiffBreath_23: int
